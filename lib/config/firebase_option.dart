7777fdcdfbf774e31d73ccd625b1cd5c3c881472
Erick Hualla <83515313+ehuallap@users.noreply.github.com>
2023-06-13 01:17:20 -0500

    Se actualizó la comprobación de errores para otros sistemas operativos

--- lib/config/firebase_option.dart
+++ lib/config/firebase_option.dart
@@ -34,6 +34,15 @@ class DefaultFirebaseOptions {
           'DefaultFirebaseOptions have not been configured for windows - '
           'you can reconfigure this by running the FlutterFire CLI again.',
         );
+      case TargetPlatform.linux:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for linux - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      default:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not supported for this platform.',
+        );
     }
   }
 }
