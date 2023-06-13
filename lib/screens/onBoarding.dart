99cfda4598bba9b6ea63022ba34f9fd23e1a349d
Moises <mcasaverde@unsa.edu.pe>
2023-06-12 23:03:17 -0500

    create onBoarding.dart

---
+++ lib/screens/onBoarding.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:lottie/lottie.dart';
+
+class OnBoarding extends StatefulWidget {
+  const OnBoarding({Key? key}) : super(key: key);
+
+  @override
+  State<OnBoarding> createState() => _OnBoardingState();
+}
