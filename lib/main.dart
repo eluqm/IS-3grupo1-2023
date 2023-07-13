07440ed24a7f98d83fd19c7cc874a2d7a88ab7e1
Miguel Deza <dmikec101@gmail.com>
2023-07-13 10:17:02 -0500

    fix files

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,55 @@
+// import 'package:dynamic_color/dynamic_color.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/material.dart';
+// import 'package:change_your_mind/screens/baymax.dart';
+// import 'package:change_your_mind/screens/login.dart';
+// import 'package:change_your_mind/screens/onBoarding.dart';
+// import 'package:change_your_mind/screens/outline.dart';
+// import 'package:change_your_mind/screens/splash.dart';
+//
+// import 'firebase_options.dart';
+//
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//
+//   //connecting the database before starting the application
+//   await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+//   );
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatefulWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+//
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return DynamicColorBuilder(
+//       builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+//         return MaterialApp(
+//           theme: ThemeData(colorScheme: lightDynamic, useMaterial3: true),
+//           darkTheme: ThemeData(colorScheme: darkDynamic, useMaterial3: true),
+//           themeMode: ThemeMode.system,
+//           debugShowCheckedModeBanner: false,
+//           initialRoute: '/',
+//           routes: {
+//             '/': (context) => const Splash(),
+//             '/onBoarding': (context) => const OnBoarding(),
+//             '/outline': (context) => const Outline(),
+//             '/login': (context) => const Login(),
+//             '/baymax': (context) => const Baymax(),
+//           },
+//         );
+//       },
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -31,7 +83,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Change_your_mind'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -58,7 +110,7 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
-    setState(() {
+    setState(() {x
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
@@ -106,7 +158,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Primer interfaz de usuario',
+              'You have pushed the button this many times:',
             ),
             Text(
               '$_counter',
