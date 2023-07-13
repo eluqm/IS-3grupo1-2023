f6cf0cf1c9752d5de6f4a55c60775084f89d0e16
Angel Choquehuanca Anconeyra <84652181+AngelAnconeyra@users.noreply.github.com>
2023-07-13 13:22:08 -0500

    Verifica que tipo de actividad será

--- test/data/model/activity_mode.dart
+++ test/data/model/activity_mode.dart
@@ -5,4 +5,28 @@ class ActivityModel {
     required this.participants,
     required this.link
   });
+  String activity;
+  String type;
+  int participants;
+  String link;
+  int backgroundType = 0;
+
+  factory ActivityModel.fromJson(Map<String, dynamic> json){
+    return ActivityModel(
+        activity: json["activity"],
+        type: json["type"],
+        participants: json["participants"],
+        link: json["link"] ?? "",
+    );
+  }
+
+  setupBackgroundType(ActivityModel activityModel) {
+    if (activityModel.type == "education" || activityModel.type == "cooking" || activityModel.type == "recreational"){
+      activityModel.backgroundType = 0;
+    } else if (activityModel.type == "music" || activityModel.type == "diy" || activityModel.type == "relaxation"){
+      activityModel.backgroundType = 1;
+    } else {
+      activityModel.backgroundType = 2;
+    }
+  }
 }
